a9e1f1587695cfedd389f0740119cb28f4d069d2
MOHD TALHA <talhamd0110@gmail.com>
2026-08-30 19:38:28 +0530

    create a registerUser and loginUser in controller

---
+++ src/controller/user.controller.js
@@ -0,0 +1,158 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiError.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { User } from "../models/user.model.js"
+const genreateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
+
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+
+
+        user.refreshToken = refreshToken;
+
+        await user.save({ validateBeforeSave: false });
+        // await user.save()
+
+        return { accessToken, refreshToken };
+
+    } catch (error) {
+        console.log("TOKEN GENERATION ERROR:", error);
+
+        throw new ApiError(
+            500,
+            "Something went wrong while generating access and refresh"
+        );
+    }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+    //access info from frontend
+    //check valid
+    //check already Exist
+    //create a user data object
+    //remove pass and refresh and check user created or not 
+    //retuen response
+
+    console.log("BODY:", req.body);
+
+    const { fullName, email, username, password, role } = req.body
+    console.log("email", email)
+
+    if (
+        [email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All field are invalid")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+
+    if (existedUser) {
+        throw new ApiError(400, "username and email are already existed")
+    }
+
+    const user = await User.create(
+        {
+            email,
+            password,
+            fullName,
+            username: username.toLowerCase(),
+            role : "user"
+        }
+    )
+
+    console.log("user" , user);
+    
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "something went wrong while registring the user")
+    }
+
+    return res.status(200)
+        .json(new ApiResponse(200, {}, "User register SuccessFully"))
+
+})
+
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    //access login info from frotend
+    //check valid
+    //check in in db
+    //check password
+    //generate access and refresh token
+    //loginuser check by id
+    //creata a option
+    //return res
+    //with copokie and json (give id ref , refresh , access)
+
+    const { email, username, password } = req.body
+
+    
+
+    if (!(email || username)) {
+        throw new ApiError(400, "email and username is invalid")
+    }
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+   
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+    
+
+    const isPasswordvalid = await user.isPasswordCorrect(password)
+   
+    if (!isPasswordvalid) {
+        throw new ApiError(400, "password is required")
+    }
+
+    const { accessToken, refreshToken } = await genreateAccessAndRefreshToken(user._id)
+    
+
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User login Successfully"
+        ))
+})
+
+const logout = asyncHandler(async(req,res) => {
+
+    
+})
+
+const refreshAccessToken = asyncHandler(async(req,res) => {})
+
+
+const getCurrentUser = asyncHandler(async(req,res) => {})
+
+const changeCurrentPassword = asyncHandler(async(req,res) => {})
+
+// const 
+
+export { registerUser, loginUser }
