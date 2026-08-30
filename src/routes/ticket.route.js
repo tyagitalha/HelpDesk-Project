af1cc1ac4ae2bffea964946ea9b366a9c77c10da
MOHD TALHA <talhamd0110@gmail.com>
2026-08-30 20:46:02 +0530

    add delete route in Ticket Router

--- src/routes/ticket.route.js
+++ src/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTicket, getAllTicket, getOneTicket, updateTicket } from "../controller/ticket.controller.js";
+import { createTicket, daleteTicket, getAllTicket, getOneTicket, updateTicket } from "../controller/ticket.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -8,5 +8,6 @@ router.route("/createTicket").post(verifyJWT, createTicket)
 router.route("/getAllTicket").get(verifyJWT, getAllTicket)
 router.route("/getTicket").get(verifyJWT, getOneTicket)
 router.route("/update/:ticketId").patch(verifyJWT, updateTicket)
+router.route("/delete/:ticketId").delete(verifyJWT, daleteTicket)
 
 export default router
