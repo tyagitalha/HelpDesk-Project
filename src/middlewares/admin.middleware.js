f38ee334b42461fa561652b22c75ac19592e9e28
MOHD TALHA <talhamd0110@gmail.com>
2026-08-30 21:56:28 +0530

    refactor: remove commented-out code in isAdmin middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -2,12 +2,7 @@ import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/apiError.js"
 
 export const isAdmin = async (req, res, next) => {
-    // const user = User.findById(User._id)
-
-    // if (user.role.enum !== "Admin") {
-    //     throw new ApiError(404, "User not Admin")
-    // }.
-
+ 
     try {
 
         if (req.user?.role !== "Admin") {
