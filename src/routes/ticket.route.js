9af68e2b467e62c50fba2e0e1c55b6c5f3e17b99
MOHD TALHA <talhamd0110@gmail.com>
2026-09-01 20:01:30 +0530

    fix: correct route handler for getTicket and ensure proper ticket retrieval

--- src/routes/ticket.route.js
+++ src/routes/ticket.route.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { createTicket, daleteTicket, getAllTicket, getOneTicket, updateTicket } from "../controller/ticket.controller.js";
+import { createTicket, daleteTicket, getAllTicket, getOneTicket, getTicket, updateTicket } from "../controller/ticket.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+// import { isAdmin } from "../middlewares/admin.middleware.js";
 
 const router = Router()
 
 router.route("/createTicket").post(verifyJWT, createTicket)
 router.route("/getAllTicket").get(verifyJWT, getAllTicket)
-router.route("/getTicket").get(verifyJWT, getOneTicket)
+router.route("/getTicket").get(verifyJWT, getTicket)
 router.route("/update/:ticketId").patch(verifyJWT, updateTicket)
 router.route("/delete/:ticketId").delete(verifyJWT, daleteTicket)
+router.route("/getOneTicket/:ticketId").get(verifyJWT, getOneTicket)
 
 export default router
