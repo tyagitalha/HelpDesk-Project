2da9a218245c4fd2eaf41c017e5823c6936eaae6
MOHD TALHA <talhamd0110@gmail.com>
2026-09-05 16:39:03 +0530

    fix: correct typo in deleteTicket function import

--- src/routes/ticket.route.js
+++ src/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTicket, daleteTicket, getAllTicket, getOneTicket, getTicket, updateTicket } from "../controller/ticket.controller.js";
+import { createTicket, deleteTicket, getAllTicket, getOneTicket, getTicket, updateTicket } from "../controller/ticket.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 // import { isAdmin } from "../middlewares/admin.middleware.js";
 
@@ -9,7 +9,7 @@ router.route("/createTicket").post(verifyJWT, createTicket)
 router.route("/getAllTicket").get(verifyJWT, getAllTicket)
 router.route("/getTicket").get(verifyJWT, getTicket)
 router.route("/update/:ticketId").patch(verifyJWT, updateTicket)
-router.route("/delete/:ticketId").delete(verifyJWT, daleteTicket)
+router.route("/delete/:ticketId").delete(verifyJWT, deleteTicket)
 router.route("/getOneTicket/:ticketId").get(verifyJWT, getOneTicket)
 
 export default router
